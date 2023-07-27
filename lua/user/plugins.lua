397f8298f6d794a1378536c22712e517e3a29340
cchun-offworld <colin.chun@offworld.ai>
2023-07-27 16:04:14 -0700

    Added diffview

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -93,6 +93,8 @@ return packer.startup(function(use)
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
 
   use { "mechatroner/rainbow_csv" }
+
+  use { "sindrets/diffview.nvim" }
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
