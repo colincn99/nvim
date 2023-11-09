a72e327b6e739f823423bb85aa32d3749b86c84d
cchun-offworld <colin.chun@offworld.ai>
2023-11-08 16:32:00 -0800

    Changed Search highlight color and made tree adaptive size

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -4,3 +4,9 @@ local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
+
+local status_ok, _ = pcall(vim.cmd, "hi Search guibg=Red")
+if not status_ok then
+  return
+end
