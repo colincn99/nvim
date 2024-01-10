caef05562980863f591cf5a5d9e2a9a302c862e8
cchun-offworld <colin.chun@offworld.ai>
2024-01-09 16:29:22 -0800

    Added <C-u> to bn bp keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":<C-u>bnext<CR>", opts)
+keymap("n", "<S-h>", ":<C-u>bprevious<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", ":m .+1<CR>==", opts)
